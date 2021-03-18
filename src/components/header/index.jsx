48086398b375f86151c23cb1b47764187586c0d4
dasoop <dasoop.official@gmail.com>
2021-03-18 22:04:07 +0900

    사회적기업 추가, 오타수정

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,11 +7,14 @@ export const Header = ({ title, location, rootPath }) => {
   const isRoot = location.pathname === rootPath
   return (
     isRoot && (
+      <>
+      <span style={{paddingLeft:13,fontSize:12}}>사회적 기업</span>
       <h1 className="home-header">
         <Link to={`/`} className="link">
           {title}
         </Link>
       </h1>
+      </>
     )
   )
 }
